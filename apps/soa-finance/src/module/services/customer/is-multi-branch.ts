125466a43a9f094856ae1581a77f7359165dd0c2
tob-david <gerardus.david@tob-ins.com>
2026-02-05 18:34:19 +0700

    delete(soa-finance): footer header and mulit branch function

--- apps/soa-finance/src/module/services/customer/is-multi-branch.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Check if customer is multi-branch
- */
-
-import { multiBranchCodes } from "../../utils/types/soa";
-
-export const isMultiBranchCustomer = (actingCode: string): boolean =>
-  multiBranchCodes.includes(actingCode);

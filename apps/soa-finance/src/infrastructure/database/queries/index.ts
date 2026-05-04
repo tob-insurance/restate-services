b30abbe2dab4d7510cf66da141d346b4e8f00f2a
jveko <github@pazmore.com>
2026-05-04 18:51:46 +0700

    chore(soa): remove dead letter-number.generator and letter-query after migration

--- apps/soa-finance/src/infrastructure/database/queries/index.ts
+++ apps/soa-finance/src/infrastructure/database/queries/index.ts
@@ -1,4 +1,3 @@
 export * from "./branch-query";
 export * from "./customer-query";
-export * from "./letter-query";
 export * from "./reminder-query";
--- apps/soa-finance/src/modules/document-generation/index.ts
+++ apps/soa-finance/src/modules/document-generation/index.ts
@@ -1,7 +1,7 @@
 export { formatTimestampToDate, generateExcel } from "./excel.generator";
 export { generateAndUploadDocuments } from "./generate-and-upload";
 export { generateSoaPdfHandler } from "./generate-soa-pdf";
-export { generateLetterNumber } from "./letter-number.generator";
+
 export { getFooter, getHeader, getSignature } from "./pdf-assets";
 export { renderLiquidToHtml } from "./pdf-render";
 export { buildPdfTemplateData } from "./pdf-template";

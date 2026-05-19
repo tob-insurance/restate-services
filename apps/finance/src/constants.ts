4b52819c553aaa517761add9de2ccf6fe0a62eda
alfasean <alfasean22@gmail.com>
2026-05-19 16:49:31 +0700

    :sparkles:feat(genius closing): change schedule time

--- apps/finance/src/constants.ts
+++ apps/finance/src/constants.ts
@@ -2,4 +2,4 @@ export const TIMEZONE = "Asia/Jakarta";
 
 export const DEFAULT_USER_ID = "adm";
 
-export const DAILY_CLOSING_SCHEDULE_TIME = "14:37";
+export const DAILY_CLOSING_SCHEDULE_TIME = "23:00";
